9ada2c86db3798d2405e2215262f38e6871b09aa
Juawo <joaopedrojp2703@gmail.com>
2025-08-14 21:38:04 -0300

    Add heap implementation and configuration files

--- heap.c
+++ heap.c
@@ -1,2 +1,57 @@
 #include "heap.h"
 
+// [6,9,2,8] -> [9,8,6,2]
+void *maxheap(int *array, int size)
+{
+    printf("...");
+}
+
+bool insert(int *heap, int value)
+{
+    return false;
+}
+
+int get_min(int *heap)
+{
+    return 0;
+}
+
+int get_max(int *heap)
+{
+    return 0;
+}
+
+int get_index(int *heap, int value)
+{
+    return 0;
+}
+
+int extract_min(int *heap)
+{
+    return 0;
+}
+
+int extract_max(int *heap)
+{
+    return 0;
+}
+
+void update(int *heap, int old_value, int value)
+{
+
+}
+
+void swap_up(int *heap, int index_value)
+{
+
+}
+
+void swap_down(int *heap, int index_value)
+{
+
+}
+
+int *heap_sort(int *array)
+{
+    return NULL;
+}
--- heap.h
+++ heap.h
@@ -3,17 +3,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-// Build an heap from a array
-int* build(int* array);
+// Build an maxheap from a array
+void maxheap(int* array, int size);
 bool insert(int *heap, int value);
-// Get min/max value in a heap, just return the value
-int get_min(int *heap);
+// Get max value in a heap, just return the value
 int get_max(int *heap);
-// Extract min/max value in a heap, remove and return the value
-int extract_min(int *heap);
+// Get the index of a value in a heap
+int get_index(int* heap, int value);
+// Extract max value in a heap, remove and return the value
 int extract_max(int *heap);
-
-
+// Update value in a heap
+void update(int* heap, int old_value, int value);
+// Make the "swap" for up/down in the values of a heap
+void swap_up(int* heap, int index_value);
+void swap_down(int* heap, int index_value);
+// Make the heapsort in a array
+int *heap_sort(int *array);
 
 #endif
